6dce19a11fe143ec9246b534aeef92b519b246fc
Dmitry <ddpaimon@gmail.com>
2017-08-21 17:50:02 +0700

    Add complete string to load.sh

--- scripts/load.sh
+++ scripts/load.sh
@@ -38,3 +38,18 @@ sleep 2
 curl -s --request POST "http://$address/v1/modules/output-streaming/pingstation-output/1.0/instance" -H 'Content-Type: application/json' --data "@api-json/instances/pingstation-output.json"
 sleep 2
 curl -s --request PUT "http://192.168.50.52:9200/pingstation" -H 'Content-Type: application/json' --data "@api-json/elasticsearch-index.json"
+
+echo ""
+echo "Complete!"
+echo "Marathon url: http://0.0.0.0:8080/"
+echo "Mesos Master url: http://0.0.0.0:5050/"
+echo "Mesos Agent url: http://0.0.0.0:5051/"
+echo "Sj-rest url: http://0.0.0.0:8888/"
+echo "Elasticsearch url: http://0.0.0.0:9200/"
+echo "Kibana url: http://0.0.0.0:5601/"
+echo ""
+echo "Additional"
+echo "Mongo address: 0.0.0.0:27017"
+echo "Cassandra address: 0.0.0.0:9042"
+echo "Kafka address: 0.0.0.0:9092"
+echo "Aerospike address: 0.0.0.0:3000"
